13179345651ead712297e4383ed16fc904ee48c0
Fachrifat <fachrifat05@gmail.com>
2024-11-17 14:50:15 +0700

    deploy vercel

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use("/food", foodRoutes);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
